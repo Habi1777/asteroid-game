fbc8aab39659fd4b0a042382a1174f703b46fe25
Habi1777 <144575722+Habi1777@users.noreply.github.com>
2025-08-20 17:02:37 -0700

    added player methods for rotation

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import pygame
 from constants import *
+from player import *
 
 def main():
     pygame.init()
@@ -8,13 +9,17 @@ def main():
     print("Starting Asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}\nScreen height: {SCREEN_HEIGHT}")
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    player_1 = Player(SCREEN_WIDTH/2, SCREEN_HEIGHT/2)
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+        player_1.update(dt)
         screen.fill((0 , 0, 0))
+        player_1.draw(screen)
         pygame.display.flip()
-        dt = game_clock.tick(60)
+        dt = game_clock.tick(60) / 1000
+
 
 if __name__ == "__main__":
     main()
